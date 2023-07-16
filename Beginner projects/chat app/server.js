e6be2701fbebecd515c70ecd15ebcff0bab78c75
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2023-07-16 10:07:05 -0500

    Chat application created

--- Beginner projects/chat app/server.js
+++ Beginner projects/chat app/server.js
@@ -1,36 +1,65 @@
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(bodyParser.urlencoded({extended: true}));
+// For static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+const botName = 'ChatCord Bot';
 
-app.get("/", (req, res) => {
-    res.render("index");
+// Run when the client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord app'));
+
+        // Broadcast when user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+    // Runs when the client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+    });
 });
 
-app.post("/form", (req, res) => {
-    const name = req.body.personName;
-    const room = req.body.roomType;
 
-    res.redirect(`/${room}?name=${name}`);
+server.listen(3000, () => {
+    console.log("The app is listening at port 3000");
 });
 
-app.get("/:room", (req, res) => {
-    const room = req.params.room;
-    const name = req.query.name;
-
-    res.render("room", { room, name });
-});
-
-
-app.listen(3000, () => {
-    console.log("Server started at port 3000");
-});

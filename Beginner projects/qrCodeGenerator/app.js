d9ac1d49f1993e2d6831994cddc0d15d543dc50c
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2023-07-16 14:58:56 -0500

    QR Code project completed

--- Beginner projects/qrCodeGenerator/app.js
+++ Beginner projects/qrCodeGenerator/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const path = require('path');
-var QRCode = require('qrcode')
+var qrCode = require('qrcode');
 
 const app = express();
 
@@ -12,6 +12,18 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/scan", (req, res) => {
+    const text = req.body.text;
+    qrCode.toDataURL(text, function (err, url) {
+        res.render("scanner", {
+            qr_code: url
+        });
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server running at port 3000");

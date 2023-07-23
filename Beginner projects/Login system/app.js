e07386a799e23462303f0981ce99267ac99ec876
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2023-07-23 12:06:45 -0500

    login system with session and cookies completed

--- Beginner projects/Login system/app.js
+++ Beginner projects/Login system/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const { Console } = require('console');
 
 const app = express();
 
@@ -11,6 +16,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/loginDataDB");
+
+const loginDataSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+loginDataSchema.plugin(passportLocalMongoose, {
+    username: 'username'
+});
+
+
+const Login = new mongoose.model("Login", loginDataSchema);
+
+passport.use(Login.createStrategy());
+
+passport.serializeUser(Login.serializeUser());
+passport.deserializeUser(Login.deserializeUser());
+
+
 app.get("/", (req, res) => {
     res.render("login");
 });
@@ -19,8 +53,57 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.get("/success", (req, res) => {
+    res.render("success");
+});
+
 app.post("/login", (req, res) => {
-    
+    const login = new Login({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(login, (err) => {
+        if(err){
+            console.log(err);
+        } else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/success");
+            });
+        }
+    });
+});
+
+app.get("/dashboard", (req, res) => {
+    if(req.isAuthenticated()){
+        res.render("success");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.post("/register", (req, res) => {
+    Login.register({ username: req.body.username }, req.body.password, function(err, user){
+        if(err){
+            console.error("Error during registration:", err);
+            res.redirect("/register");
+        } else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/dashboard");
+            });
+        }
+    });
+});
+
+app.post("/logout", (req, res) => {
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    });
+    // res.redirect("/");
 });
 
 app.listen(3000, (req, res) => {
